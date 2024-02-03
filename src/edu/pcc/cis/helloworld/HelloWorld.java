db2fcca84b0f0d78d500c2b1c0f2aff1bbd6369d
Eric Stallings <eric.stallings@pcc.edu>
2024-02-03 09:17:06 -0800

    Added comments header to HelloWorld

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,3 +1,12 @@
+/**
+ * This is a simple class to say Hello, World. This class requires improvement.
+ *
+ * Modifications:
+ * ES:
+ *
+ * @author Eric Stallings
+ * @version 2024.02.06
+ */
 package edu.pcc.cis.helloworld;
 
 public class HelloWorld {
